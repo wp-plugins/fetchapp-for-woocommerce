779d980f3653c7b4e693475fc97d405af4c737b1
prcapps <prcapps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-03 21:58:09 +0000

    Created 1.0.1 tag, updated plugin file version.
    git-svn-id: https://plugins.svn.wordpress.org/fetchapp-for-woocommerce/trunk@766555 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fetchapp.php
+++ fetchapp.php
@@ -4,7 +4,7 @@ Plugin Name: FetchApp
 Plugin URI: http://www.fetchapp.com/
 Description: Fetch App Integration for WooCommerce
 Author: Patrick Conant
-Version: 1.0
+Version: 1.0.1
 Author URI: http://www.prcapps.com/
 */
 
