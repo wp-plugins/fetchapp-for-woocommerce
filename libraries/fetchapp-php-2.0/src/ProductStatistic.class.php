5f99a9280957fae06d1f8ee982cb6d7dd60967b1
prcapps <prcapps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-03 21:46:57 +0000

    Updated menu position to fix issue reported in support request.
    git-svn-id: https://plugins.svn.wordpress.org/fetchapp-for-woocommerce/tags/1.0.1@766543 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ libraries/fetchapp-php-2.0/src/ProductStatistic.class.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * Updated by SublimeText 2.
+ * Creator: Brendon Dugan <wishingforayer@gmail.com>
+ * Last Updated: Patrick Conant <conantp@gmai.com>
+ * User: Patrick Conant <conantp@gmai.com>
+ * Date: 8/7/13
+ * Time: 8:00 PM
+ */
+
+namespace FetchApp\API;
+
+
+class ProductStatistic
+{
+    /**
+     * @var $ProductID String
+     */
+    private $ProductID;
+
+    /**
+     * @var $SKU String
+     */
+    private $SKU;
+
+    /**
+     * @var $DownloadCount int
+     */
+    private $DownloadCount;
+
+    /**
+     * @var $OrderCount int
+     */
+    private $OrderCount;
+
+    /**
+     * @var $Price float
+     */
+    private $Price;
+
+    /**
+     * @var int Currency
+     */
+    private $Currency;
+
+    /**
+     * @param int $Currency
+     */
+    public function setCurrency($Currency)
+    {
+        $this->Currency = $Currency;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrency()
+    {
+        return $this->Currency;
+    }
+
+    /**
+     * @param int $DownloadCount
+     */
+    public function setDownloadCount($DownloadCount)
+    {
+        $this->DownloadCount = $DownloadCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDownloadCount()
+    {
+        return $this->DownloadCount;
+    }
+
+    /**
+     * @param String $ProductID
+     */
+    public function setProductID($ProductID)
+    {
+        $this->ProductID = $ProductID;
+    }
+
+    /**
+     * @return String
+     */
+    public function getProductID()
+    {
+        return $this->ProductID;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice($Price)
+    {
+        $this->Price = $Price;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->Price;
+    }
+
+    /**
+     * @param int $ProductCount
+     */
+    public function setOrderCount($OrderCount)
+    {
+        $this->OrderCount = $OrderCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrderCount()
+    {
+        return $this->OrderCount;
+    }
+
+    /**
+     * @param String $SKU
+     */
+    public function setSKU($SKU)
+    {
+        $this->SKU = $SKU;
+    }
+
+    /**
+     * @return String
+     */
+    public function getSKU()
+    {
+        return $this->SKU;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Plugin Name ===
+Contributors: prcapps
+Donate link: http://www.prcapps.com/
+Tags: fetchapp,woocommerce
+Requires at least: 3.6
+Tested up to: 3.6
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin provides integration between FetchApp and WooCommerce.
+
+== Description ==
+
+This plugin provides integration between FetchApp and WooCommerce.
+
+<h3>Features:</h3>
+<ul>
+<li>Pull products and orders from FetchApp into WooCommerce</li>
+<li>Push products and orders from WooCommerce up to FetchApp</li>
+<li>Automatic synchronization of new orders placed through your store, including the email of product download links</li>
+<li>Periodic synchronization using a scheduled WordPress task.</li>
+</ul>
+
+== Installation ==
+
+1. Install the plugin.
+1. Select the FetchApp section of the administrative panel, and enter your FetchApp API token and key
+1. Synchronize your products by click the "Synchronize Products" or "Synchronize Orders" buttons
+
+== Changelog ==
+
+= 1.0 =
+* Inital Release
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Inital Release
+
